fc2e751bd0e2b11a3f1df5abfd438febcb606ed7
Sarah Mae Centino <2152834@slu.edu.ph>
2018-05-20 16:11:48 +0800

    updated

---
+++ website/pages/developers.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>WebTech</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+    
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="../css/animate.css">
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="../css/icomoon.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <!-- Flexslider  -->
+    <link rel="stylesheet" href="../css/flexslider.css">
+    <!-- Flexslider  -->
+    <link rel="stylesheet" href="../css/flexslider.css">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="../css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="icon" href="../images/webtek.png">
+ 
+</head>
+
+<body>
+    <nav id="colorlib-main-nav" role="navigation">
+        <a class="js-colorlib-nav-toggle colorlib-nav-toggle active"><i></i></a>
+        <div class="js-fullheight colorlib-table">
+            <div class="colorlib-table-cell js-fullheight">
+                <ul>
+                    <li><a href="../home.html">Home</a></li>
+                    <li><a href="pages/notes.html">Notes</a></li>
+                    <li><a href="pages/quiz.html">Quiz</a></li>
+                    <li><a href="pages/video.html">Video Tutorials</a></li>
+                    <li><a href="pages/developers.html">Developers</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div id="colorlib-page">
+        <header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <a class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <aside id="colorlib-hero">
+            <div class="flexslider">
+                <ul class="slides">
+                <li style="background-image: url(../images/slide5.jpg);">
+                    <div class="overlay"></div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-8 col-sm-12 col-md-offset-2 col-md-pull-2 slider-text">
+                                <div class="slider-text-inner">
+                                    <h2>IT 324</h2>
+                                    <h1>Web Systems And Technologies</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li style="background-image: url(../images/slide6.png);">
+                    <div class="overlay"></div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-8 col-sm-12 col-md-offset-2 col-md-pull-2 slider-text">
+                                <div class="slider-text-inner">
+                                    <h2>Sharing your knowledge with others,</h2>
+                                    <h1>Does not make you less important.</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li style="background-image: url(../images/slide4.png);">
+                    <div class="overlay"></div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-8 col-sm-12 col-md-offset-2 col-md-pull-2 slider-text">
+                                <div class="slider-text-inner">
+                                    <h2>Sites need to be able to interact in one single, universal space.</h2>
+                                    <h1> - Tim Berners-Lee </h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                </ul>
+            </div>
+        </aside>
+
+<div id="colorlib-testimony" class="colorlib-light-grey">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center animate-box intro-heading">
+						<h2>DEVELOPERS</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="row animate-box">
+							<div class="owl-carousel1">
+								<div class="item">
+									<div class="testimony-slide text-center active">
+										<figure>
+											<img src="../images/dianne.png" alt="user">
+										</figure>
+										<blockquote>
+											<p>Dianne Alyza M. Cayabyab</p>
+											<span>Front-End Developer</span>
+										</blockquote>
+									</div>
+								</div>
+								<div class="item">
+									<div class="testimony-slide text-center active">
+										<figure>
+											<img src="../images/sarah.jpg" alt="user">
+										</figure>
+										<blockquote>
+											<p> Sarah Mae E. Centino</p>
+											<span>Front-End Developer</span>
+										</blockquote>
+									</div>
+								</div>
+								<div class="item">
+									<div class="testimony-slide text-center active">
+										<figure>
+											<img src="../images/graham.png" alt="user">
+										</figure>
+										<blockquote>
+											<p>Graham Ross H. Flora</p>
+											<span>Back-End Developer</span>
+										</blockquote>
+									</div>
+								</div>
+								<div class="item">
+									<div class="testimony-slide text-center active">
+										<figure>
+											<img src="../images/lovelace.png" alt="user">
+										</figure>
+										<blockquote>
+											<p>Lovelace Zennia Luisa R. Oliva</p>
+											<span>Back-End Developer</span>
+										</blockquote>
+									</div>
+								</div>
+								<div class="item">
+									<div class="testimony-slide text-center active">
+										<figure>
+											<img src="../images/person3.jpg" alt="user">
+										</figure>
+										<blockquote>
+											<p>John Yves B. Ragsac</p>
+											<span></span>
+										</blockquote>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	<footer>
+			<div id="footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4 col-pb-sm">
+							<div class="row">
+								<div class="col-md-10">
+									<h2>Web Systems and Technologies</h2>
+									<p>9325A | 1:00 - 2:00 TF</p>
+									<span class="block">
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved <br></span>  
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	
+	</div>
+	<script src="../js/lib/jquery/jquery.min.js"></script>
+ <script src="../js/modernizr-2.6.2.min.js"></script>
+	<!-- jQuery -->
+	
+	<!-- jQuery Easing -->
+	<script src="../js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="../js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="../js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="../js/jquery.flexslider-min.js"></script>
+	<!-- Counters -->
+	<script src="../js/jquery.countTo.js"></script>
+	<!-- Owl Carousel -->
+	<script src="../js/owl.carousel.min.js"></script>
+
+	<!-- Main JS (Do not remove) -->
+	<script src="../js/main.js"></script>
+
+
+</body>
+</html>
---
+++ website/pages/video.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Webtek</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+    
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="../css/animate.css">
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="../css/icomoon.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <!-- Flexslider  -->
+    <link rel="stylesheet" href="../css/flexslider.css">
+    <!-- Flexslider  -->
+    <link rel="stylesheet" href="../css/flexslider.css">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="../css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="icon" href="../images/webtek.png">
+ 
+</head>
+
+<body>
+    <nav id="colorlib-main-nav" role="navigation">
+        <a class="js-colorlib-nav-toggle colorlib-nav-toggle active"><i></i></a>
+        <div class="js-fullheight colorlib-table">
+            <div class="colorlib-table-cell js-fullheight">
+                <ul>
+                    <li><a href="../home.html">Home</a></li>
+                    <li><a href="notes.html">Notes</a></li>
+                    <li><a href="quiz.html">Quiz</a></li>
+                    <li><a href="video.html">Video Tutorials</a></li>
+                    <li><a href="developers.html">Developers</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div id="colorlib-page">
+        <header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <a class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <aside id="colorlib-hero">
+            <div class="flexslider">
+                <ul class="slides">
+                <li style="background-image: url(../images/slide5.jpg);">
+                    <div class="overlay"></div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-8 col-sm-12 col-md-offset-2 col-md-pull-2 slider-text">
+                                <div class="slider-text-inner">
+                                    <h2>IT 324</h2>
+                                    <h1>Web Systems And Technologies</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li style="background-image: url(../images/slide6.png);">
+                    <div class="overlay"></div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-8 col-sm-12 col-md-offset-2 col-md-pull-2 slider-text">
+                                <div class="slider-text-inner">
+                                    <h2>Sharing your knowledge with others,</h2>
+                                    <h1>Does not make you less important.</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li style="background-image: url(../images/slide4.png);">
+                    <div class="overlay"></div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-8 col-sm-12 col-md-offset-2 col-md-pull-2 slider-text">
+                                <div class="slider-text-inner">
+                                    <h2>Sites need to be able to interact in one single, universal space.</h2>
+                                    <h1> - Tim Berners-Lee </h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                </ul>
+            </div>
+        </aside>
+<div id="colorlib-blog">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 col-md-offset-0">
+						<article class="blog-entry animate-box">
+							<div class="blog-wrap">
+								<div class="blog-image">
+									<a href="blog.html" class="blog-img text-center" style="background-image: url(assets/images/blog-1.jpg);"><span><i class="icon-link"></i></span></a>
+								</div>
+								<h2 class="text-center"><a href="blog.html">Little Blind Text should turn around</a></h2>	
+							</div>
+							<div class="desc">
+								<span class="category"><a href="blog.html"><i class="icon-calendar2"></i> January 27, 2017</a> <a href="blog.html" class="posted-by"><i class="icon-user2"></i> by Stephy</a> <a href="blog.html"><i class="icon-bubble3"></i> 5 Comments</a></span>
+								<p class="first-letra">Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
+							</div>
+							<p class="text-center"><a href="blog.html" class="btn btn-primary btn-custom">Continue Reading</a></p>
+						</article>
+						<article class="blog-entry animate-box">
+							<div class="blog-wrap">
+								<div class="blog-image">
+									<a href="blog.html" class="blog-img text-center" style="background-image: url(assets/images/blog-3.jpg);"><span><i class="icon-link"></i></span></a>
+								</div>
+								<h2 class="text-center"><a href="blog.html">Little Blind Text should turn around</a></h2>
+							</div>
+							<div class="desc">
+								<span class="category"><a href="blog.html"><i class="icon-calendar2"></i> January 27, 2017</a> <a href="blog.html" class="posted-by"><i class="icon-user2"></i> by Stephy</a> <a href="blog.html"><i class="icon-bubble3"></i> 5 Comments</a></span>
+								<p class="first-letra">Her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country. But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their.</p>
+							</div>
+							<p class="text-center"><a href="blog.html" class="btn btn-primary btn-custom">Continue Reading</a></p>
+						</article>
+						<article class="blog-entry animate-box">
+							<div class="blog-wrap">
+								<div class="blog-image">
+									<a href="blog.html" class="blog-img text-center" style="background-image: url(assets/images/blog-4.jpg);"><span><i class="icon-link"></i></span></a>
+								</div>
+								<h2 class="text-center"><a href="blog.html">Little Blind Text should turn around</a></h2>
+							</div>
+							<div class="desc">
+								<span class="category"><a href="blog.html"><i class="icon-calendar2"></i> January 27, 2017</a> <a href="blog.html" class="posted-by"><i class="icon-user2"></i> by Stephy</a> <a href="blog.html"><i class="icon-bubble3"></i> 5 Comments</a></span>
+								<p class="first-letra">But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their. When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way.
+</p>
+							</div>
+							<p class="text-center"><a href="blog.html" class="btn btn-primary btn-custom">Continue Reading</a></p>
+						</article>
+						<article class="blog-entry animate-box">
+							<div class="blog-wrap">
+								<div class="blog-image">
+									<a href="blog.html" class="blog-img text-center" style="background-image: url(assets/images/blog-5.jpg);"><span><i class="icon-link"></i></span></a>
+								</div>
+								<h2 class="text-center"><a href="blog.html">Little Blind Text should turn around</a></h2>
+							</div>
+							<div class="desc">
+								<span class="category"><a href="blog.html"><i class="icon-calendar2"></i> January 27, 2017</a> <a href="blog.html" class="posted-by"><i class="icon-user2"></i> by Stephy</a> <a href="blog.html"><i class="icon-bubble3"></i> 5 Comments</a></span>
+								<p class="first-letra">The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country. But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their.</p>
+							</div>
+							<p class="text-center"><a href="blog.html" class="btn btn-primary btn-custom">Continue Reading</a></p>
+						</article>
+						<div class="row">
+							<div class="col-md-12 text-center">
+								<ul class="pagination">
+									<li class="disabled"><a href="#">&laquo;</a></li>
+									<li class="active"><a href="#">1</a></li>
+									<li><a href="#">2</a></li>
+									<li><a href="#">3</a></li>
+									<li><a href="#">4</a></li>
+									<li><a href="#">&raquo;</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+<script src="../js/lib/jquery/jquery.min.js"></script>
+ <script src="../js/modernizr-2.6.2.min.js"></script>
+	<!-- jQuery -->
+	
+	<!-- jQuery Easing -->
+	<script src="../js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="../js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="../js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="../js/jquery.flexslider-min.js"></script>
+	<!-- Counters -->
+	<script src="../js/jquery.countTo.js"></script>
+	<!-- Owl Carousel -->
+	<script src="../js/owl.carousel.min.js"></script>
+
+	<!-- Main JS (Do not remove) -->
+	<script src="../js/main.js"></script>
+
+
+</body>
+</html>
