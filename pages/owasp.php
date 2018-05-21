055e551bb3e551417e88396ab55beedd8c60bb79
Sarah Mae Centino <2152834@slu.edu.ph>
2018-05-21 20:27:15 +0800

    edited owasp notes

--- pages/owasp.php
+++ pages/owasp.php
@@ -13,7 +13,7 @@
 	<link rel="shortcut icon" href="favicon.ico">
 
 	<link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,700" rel="stylesheet">
-	
+
 	<!-- Animate.css -->
 	<link rel="stylesheet" href="../css/animate.css">
 	<!-- Icomoon Icon Fonts-->
@@ -29,7 +29,8 @@
 	<link rel="stylesheet" href="../css/owl.theme.default.min.css">
 
 	<link rel="stylesheet" href="../css/style.css">
-	
+	<link rel="stylesheet" href="../css/tab.css">
+
 	<!-- Modernizr JS -->
 	<script src="../js/modernizr-2.6.2.min.js"></script>
 	<!-- FOR IE9 below -->
@@ -39,17 +40,16 @@
 
 	</head>
 	<body>
-	
 		<aside id="colorlib-hero">
 			<div class="flexslider">
 				<ul class="slides">
-			   	<li style="background-image: url(images/cover_img_4.jpg);">
+			   	<li style="background-image: url(../images/cover_img_4.jpg);">
 			   		<div class="overlay"></div>
 			   		<div class="container">
 			   			<div class="row">
 				   			<div class="col-md-8 col-sm-12 col-xs-12 col-md-offset-2 text-center slider-text">
 				   				<div class="slider-text-inner">
-				   					<h1>Scoped Objects</h1>
+				   					<h1>OWASP Top 10 Web Application Security Risks</h1>
 				   				</div>
 				   			</div>
 				   		</div>
@@ -60,42 +60,57 @@
 		</aside>
 		<div id="colorlib-work">
 			<div class="container">
-				<div class="row row-padded-bottom">
-					<div class="col-md-6 animate-box">
-					<h1>Scoped Objects(in Java Web)</h1>
-					<p>-Object given a name and exposed in a given scope.</p>
-
-					<h1>Types of Scoped Object:</h1>
-					<h2>1. Context Scope</h2>
-					<p>-Starts from the point where a web application is put into service (started) till it is removed from service (shutdown) or the web application is reloaded. Parameters/attributes within the application scope will be available to all requests and sessions.</p> 
-					<p>-Available to everything as long as the application is running</p>
-					<p>-Represents by Servlet Context</p>
-				    <h2>2. Request Scope</h2>
-				    <p>-Start from the moment an HTTP request hits a servlet in our web container and end when the servlet is done with delivering the HTTP response.</p>
-
-						
+				<button class="accordion">Injection</button>
+					<div class="panel">
+	  					<p>Injection flaws, such as SQL, NoSQL, OS, and LDAP injection, occur when untrusted data is sent to an interpreter as part of a command or query. The attacker's hostile data can trick the interpreter into executing unintended commands or accessing data without proper authorization.</p>
 					</div>
-					<div class="col-md-6 col-md-push-1 animate-box">
-						<div class="work-detail">
-						<h3>3. Session Scope</h3>
-						<p>-A session scope starts when a client (e.g. browser window) establishes connection with our web application till the point where the browser window is closed.</p>
-						<p>-Session scope spans across multiple requests from the same client.</p>
-                   		<p>-Session scope is denoted by javax.servlet.http.HttpSession interface.</p>
-						<p>-Session object is available in a JSP page as an implicit object called session.</p> 
-						<p>-In a servlet, you can get Session object by calling request.getSession().</p>
-						<h3>4. JSP page Scope</h3>
-						<p>-The page scope restricts the scpoe and lifetime of attributes to the same page where it was created.</p>
-						<p>-It is available in a JSP page as an implicit object called pageScope.</p>
 
-							
-
+				<button class="accordion">Broken Authentication</button>
+					<div class="panel">
+	  					<p>Application functions related to authentication and session management are often implemented incorrectly, allowing attackers to compromise passwords, keys, or session tokens, or to exploit other implementation flaws to assume other users' identities temporarily or permanently.</p>
 					</div>
-				</div>
-			
-		  </div>
-				
+
+				<button class="accordion">Sensitive Data Exposure</button>
+					<div class="panel">
+	  					<p>Many web applications and APIs do not properly protect sensitive data, such as financial, healthcare, and PII. Attackers may steal or modify such weakly protected data to conduct credit card fraud, identity theft, or other crimes. Sensitive data may be compromised without extra protection, such as encryption at rest or in transit, and requires special precautions when exchanged with the browser.</p>
+					</div>					
+
+				<button class="accordion">XML External Entities (XXE)</button>
+					<div class="panel">
+	  					<p>Many older or poorly configured XML processors evaluate external entity references within XML documents. External entities can be used to disclose internal files using the file URI handler, internal file shares, internal port scanning, remote code execution, and denial of service attacks.</p>
+					</div>	
+
+				<button class="accordion">Broken Access Control</button>
+					<div class="panel">
+	  					<p>Restrictions on what authenticated users are allowed to do are often not properly enforced. Attackers can exploit these flaws to access unauthorized functionality and/or data, such as access other users' accounts, view sensitive files, modify other users' data, change access rights, etc.</p>
+					</div>	
+
+				<button class="accordion">Security Misconfiguration</button>
+					<div class="panel">
+	  					<p>Security misconfiguration is the most commonly seen issue. This is commonly a result of insecure default configurations, incomplete or ad hoc configurations, open cloud storage, misconfigured HTTP headers, and verbose error messages containing sensitive information. Not only must all operating systems, frameworks, libraries, and applications be securely configured, but they must be patched/upgraded in a timely fashion.</p>
+					</div>	
+
+				<button class="accordion">Cross-Site Scripting (XSS)</button>
+					<div class="panel">
+	  					<p>XSS flaws occur whenever an application includes untrusted data in a new web page without proper validation or escaping, or updates an existing web page with user-supplied data using a browser API that can create HTML or JavaScript. XSS allows attackers to execute scripts in the victim's browser which can hijack user sessions, deface web sites, or redirect the user to malicious sites.</p>
+					</div>	
+
+				<button class="accordion">Insecure Deserialization</button>
+					<div class="panel">
+	  					<p>Insecure deserialization often leads to remote code execution. Even if deserialization flaws do not result in remote code execution, they can be used to perform attacks, including replay attacks, injection attacks, and privilege escalation attacks.</p>
+					</div>
+
+				<button class="accordion">Using Components with Known Vulnerabilities</button>
+					<div class="panel">
+	  					<p>Components, such as libraries, frameworks, and other software modules, run with the same privileges as the application. If a vulnerable component is exploited, such an attack can facilitate serious data loss or server takeover. Applications and APIs using components with known vulnerabilities may undermine application defenses and enable various attacks and impacts.</p>
+					</div>
+
+				<button class="accordion">Insufficient Logging and Monitoring</button>
+					<div class="panel">
+	  					<p>Insufficient logging and monitoring, coupled with missing or ineffective integration with incident response, allows attackers to further attack systems, maintain persistence, pivot to more systems, and tamper, extract, or destroy data. Most breach studies show time to detect a breach is over 200 days, typically detected by external parties rather than internal processes or monitoring.</p>
+					</div>	
+			</div>
 		</div>
-	</div>
 
 	<!-- jQuery -->
 	<script src="../js/jquery.min.js"></script>
@@ -114,6 +129,21 @@
 
 	<!-- Main JS (Do not remove) -->
 	<script src="../js/main.js"></script>
+	<script>
+	var acc = document.getElementsByClassName("accordion");
+	var i;
 
+	for (i = 0; i < acc.length; i++) {
+			acc[i].addEventListener("click", function() {
+					this.classList.toggle("active");
+					var panel = this.nextElementSibling;
+					if (panel.style.display === "block") {
+							panel.style.display = "none";
+					} else {
+							panel.style.display = "block";
+					}
+			});
+	}
+	</script>
 	</body>
 </html>
