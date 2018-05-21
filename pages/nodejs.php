f629bc25f1734dd152149fe80d7a1fe3468f0f2b
GrahamFlora <grahamrossflora@gmail.com>
2018-05-21 23:24:19 +0800

    Edited the picture and notes

--- pages/nodejs.php
+++ pages/nodejs.php
@@ -29,6 +29,7 @@
 	<link rel="stylesheet" href="../css/owl.theme.default.min.css">
 
 	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="../css/tab.css">
 
 	<!-- Modernizr JS -->
 	<script src="../js/modernizr-2.6.2.min.js"></script>
@@ -39,89 +40,68 @@
 
 	</head>
 	<body>
-
 		<aside id="colorlib-hero">
 			<div class="flexslider">
 				<ul class="slides">
-			   	<li style="background-image: url(images/cover_img_4.jpg);">
+			   	<li style="background-image: url(../images/Nodejs.png);">
 			   		<div class="overlay"></div>
-			   		<div class="container">
-			   			<div class="row">
-				   			<div class="col-md-8 col-sm-12 col-xs-12 col-md-offset-2 text-center slider-text">
-				   				<div class="slider-text-inner">
-				   					<h1>Scoped Objects</h1>
-				   				</div>
-				   			</div>
-				   		</div>
-			   		</div>
 			   	</li>
 			  	</ul>
 		  	</div>
 		</aside>
 		<div id="colorlib-work">
 			<div class="container">
-				<button class="accordion">PHP SuperGlobals variables</button>
-	<div class="panel">
-		<p>
-			<ul>
-				<li> $GLOBALS <br>
-					<p>is used to access global variables from anywhere in the PHP script (also from within functions or methods).</p>
-				</li>
-				<li> $_SERVER <br>
-					<p>is a PHP super global variable which holds information about headers, paths, and script locations.</p>
-				</li>
-				<li>$_REQUEST <br>
-					<p>is used to collect data after submitting an HTML form.</p>
-				</li>
-				<li>$_POST<br>
-					<p>is widely used to collect form data after submitting an HTML form with method="post". $_POST is also widely used to pass variables</p>
-				</li>
-				<li>$_GET<br>
-					 <p>can also be used to collect form data after submitting an HTML form with method="get". $_GET can also collect data sent in the URL</p>
-				</li>
-				<li>$_FILES</li>
-				<li>$_ENV</li>
-				<li>$_COOKIE</li>
-				<li>$_SESSION</li>
-			</ul>
-		</p>
-	</div>
+				<button class="accordion">Injection</button>
+					<div class="panel">
+	  					<p>Injection flaws, such as SQL, NoSQL, OS, and LDAP injection, occur when untrusted data is sent to an interpreter as part of a command or query. The attacker's hostile data can trick the interpreter into executing unintended commands or accessing data without proper authorization.</p>
+					</div>
 
-	<button class="accordion">Database Extensions</button>
-	<div class="panel">
-		<p>
-			Abstraction Layer:
-			<ul>
-						<li>DBA — Database ( dbm style) Abstraction Layer</li>
-						<li> dbx</li>
-						<li> ODBC — ODBC (Unified)</li>
-						<li>	PDO — PHP Data Objects</li>
-					</ul>
-		</p>
-	</div>
+				<button class="accordion">Broken Authentication</button>
+					<div class="panel">
+	  					<p>Application functions related to authentication and session management are often implemented incorrectly, allowing attackers to compromise passwords, keys, or session tokens, or to exploit other implementation flaws to assume other users' identities temporarily or permanently.</p>
+					</div>
 
-	<button class="accordion">Way to delimit strings</button>
-	<div class="panel">
-		<p>
-			<ul>
-			<li>Heredoc <br>
-				<p>- A third way to delimit strings is the heredoc syntax: <<<. After this operator, an identifier is provided, then a newline. The string itself follows, and then the same identifier again to close the quotation. </p>
-				</li>
-					<li>Nowdoc <br>
-			<p>- Are to single-quote</p>
-			</li>
-</ul>
-	</p>
-	</div>
+				<button class="accordion">Sensitive Data Exposure</button>
+					<div class="panel">
+	  					<p>Many web applications and APIs do not properly protect sensitive data, such as financial, healthcare, and PII. Attackers may steal or modify such weakly protected data to conduct credit card fraud, identity theft, or other crimes. Sensitive data may be compromised without extra protection, such as encryption at rest or in transit, and requires special precautions when exchanged with the browser.</p>
+					</div>					
 
-	<button class="accordion">Section 3</button>
-	<div class="panel">
-		<p>adad</p>
-	</div>
+				<button class="accordion">XML External Entities (XXE)</button>
+					<div class="panel">
+	  					<p>Many older or poorly configured XML processors evaluate external entity references within XML documents. External entities can be used to disclose internal files using the file URI handler, internal file shares, internal port scanning, remote code execution, and denial of service attacks.</p>
+					</div>	
 
+				<button class="accordion">Broken Access Control</button>
+					<div class="panel">
+	  					<p>Restrictions on what authenticated users are allowed to do are often not properly enforced. Attackers can exploit these flaws to access unauthorized functionality and/or data, such as access other users' accounts, view sensitive files, modify other users' data, change access rights, etc.</p>
+					</div>	
 
+				<button class="accordion">Security Misconfiguration</button>
+					<div class="panel">
+	  					<p>Security misconfiguration is the most commonly seen issue. This is commonly a result of insecure default configurations, incomplete or ad hoc configurations, open cloud storage, misconfigured HTTP headers, and verbose error messages containing sensitive information. Not only must all operating systems, frameworks, libraries, and applications be securely configured, but they must be patched/upgraded in a timely fashion.</p>
+					</div>	
+
+				<button class="accordion">Cross-Site Scripting (XSS)</button>
+					<div class="panel">
+	  					<p>XSS flaws occur whenever an application includes untrusted data in a new web page without proper validation or escaping, or updates an existing web page with user-supplied data using a browser API that can create HTML or JavaScript. XSS allows attackers to execute scripts in the victim's browser which can hijack user sessions, deface web sites, or redirect the user to malicious sites.</p>
+					</div>	
+
+				<button class="accordion">Insecure Deserialization</button>
+					<div class="panel">
+	  					<p>Insecure deserialization often leads to remote code execution. Even if deserialization flaws do not result in remote code execution, they can be used to perform attacks, including replay attacks, injection attacks, and privilege escalation attacks.</p>
+					</div>
+
+				<button class="accordion">Using Components with Known Vulnerabilities</button>
+					<div class="panel">
+	  					<p>Components, such as libraries, frameworks, and other software modules, run with the same privileges as the application. If a vulnerable component is exploited, such an attack can facilitate serious data loss or server takeover. Applications and APIs using components with known vulnerabilities may undermine application defenses and enable various attacks and impacts.</p>
+					</div>
+
+				<button class="accordion">Insufficient Logging and Monitoring</button>
+					<div class="panel">
+	  					<p>Insufficient logging and monitoring, coupled with missing or ineffective integration with incident response, allows attackers to further attack systems, maintain persistence, pivot to more systems, and tamper, extract, or destroy data. Most breach studies show time to detect a breach is over 200 days, typically detected by external parties rather than internal processes or monitoring.</p>
+					</div>	
+			</div>
 		</div>
-	</div>
 
 	<!-- jQuery -->
 	<script src="../js/jquery.min.js"></script>
@@ -140,6 +120,21 @@
 
 	<!-- Main JS (Do not remove) -->
 	<script src="../js/main.js"></script>
+	<script>
+	var acc = document.getElementsByClassName("accordion");
+	var i;
 
+	for (i = 0; i < acc.length; i++) {
+			acc[i].addEventListener("click", function() {
+					this.classList.toggle("active");
+					var panel = this.nextElementSibling;
+					if (panel.style.display === "block") {
+							panel.style.display = "none";
+					} else {
+							panel.style.display = "block";
+					}
+			});
+	}
+	</script>
 	</body>
 </html>
