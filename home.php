518b4328c2eed33f648a0cfc1e09788cf7aebe0f
Lovelace Zennia Luisa Oliva <2144074@slu.edu.ph>
2018-05-21 18:19:49 +0800

    update home

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>IT 324 | Web Systems and Technologies</title>
+    <title>Webtek</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="" />
     <meta name="keywords" content="" />
@@ -98,83 +98,64 @@
             </div>
         </aside>
 
-        <div id="colorlib-work">
-        			<div class="container-fluid">
-        				<div class="row">
-        					<div class="col-md-8 col-md-offset-2 text-center animate-box intro-heading">
-        						<h2>Overview</h2>
-        					</div>
-        				</div><div class="row row-padded-bottom">
-        					<div class="col-md-6 animate-box text-center">
-        						<a href="php.php" class="work-img" style="background-image: url(images/php.png);">
-        							<div class="overlay"></div>
-        							<div class="desc">
-
-        								<h3>PHP</h3>
+        <div id="colorlib-services">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2 text-center animate-box intro-heading">
+                <h2>Overview</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 animate-box">
+                <div class="services">
+                    <span class="icon"><i class="icon-pen3"></i></span>
+                    <div class="desc">
+                        <h3>PHP</h3>
                         <p>is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.</p>
-        							</div>
-        						</a>
-        					</div>
-        				<div class="row row-padded-bottom">
-        					<div class="col-md-6 animate-box text-center">
-        						<a href="servlet.php" class="work-img" style="background-image: url(images/servlets.png);">
-        							<div class="overlay"></div>
-        							<div class="desc">
-
-        								<h3>Java Web Servlet</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 animate-box">
+                <div class="services">
+                    <span class="icon"><i class="icon-diamond"></i></span>
+                    <div class="desc">
+                        <h3>Java Servlets</h3>
                         <p>A java program that extends the capabilities of a server.</p>
-        							</div>
-        						</a>
-        					</div>
-        					<div class="col-md-6 animate-box text-center">
-        						<a href="servlet.php" class="work-img" style="background-image: url(images/servlets.png);">
-        							<div class="overlay"></div>
-        							<div class="desc">
-        								<h3>Java Server Pages</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 animate-box">
+                <div class="services">
+                    <span class="icon"><i class="icon-paperplane"></i></span>
+                    <div class="desc">
+                        <h3>Scoped Object</h3>
                         <p>Object given a name and exposed in a given scope.</p>
-        							</div>
-        						</a>
-        					</div>
-        					<div class="col-md-6 animate-box text-center">
-        						<a href="sessionhandler.php" class="work-img" style="background-image: url(images/sh.png);">
-        							<div class="overlay"></div>
-        							<div class="desc">
-        								<h3>Session Handling</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 animate-box">
+                <div class="services">
+                    <span class="icon"><i class="icon-params"></i></span>
+                    <div class="desc">
+                        <h3>Node JS</h3>
+                        <p>An open-source, cross-platform JavaScript run-time environment that executes JavaScript code server-side.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 animate-box">
+                <div class="services">
+                    <span class="icon"><i class="icon-photo"></i></span>
+                    <div class="desc">
+                        <h3>Session Handling</h3>
                         <p>A way to store information (in variables) to be used across multiple pages.</p>
-        							</div>
-        						</a>
-        					</div>
-        					<div class="col-md-6 animate-box text-center">
-        						<a href="ScopedObjects.php" class="work-img" style="background-image: url(images/so.png);">
-        							<div class="overlay"></div>
-        							<div class="desc">
-        								<h3>Scoped Objects</h3>
-                        <p>Object given a name and exposed in a given scope.</p>
-        							</div>
-        						</a>
-        					</div>
-        					<div class="col-md-6 animate-box text-center">
-        						<a href="nodejs.php" class="work-img" style="background-image: url(images/nodejs.jpg);">
-        							<div class="overlay"></div>
-        							<div class="desc">
-        								<h3>Node JS</h3>
-                        	<p>An open-source, cross-platform JavaScript run-time environment that executes JavaScript code server-side.</p>
-        							</div>
-        						</a>
-        					</div>
-        					<div class="col-md-6 animate-box text-center">
-        						<a href="owasp.php" class="work-img" style="background-image: url(images/OWASP1.jpg);">
-        							<div class="overlay"></div>
-        							<div class="desc">
-        								<h3>OWASP Top 10 Web Application Security Risks</h3>
-
-        							</div>
-        						</a>
-        					</div>
-        				</div>
-        			</div>
-        		</div>
-        	</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 	<footer>
 			<div id="footer">
 				<div class="container">
