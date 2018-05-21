80fd70a62bb5ab0d2d486ff996ad61e5ac4adc9e
Sarah Mae Centino <2152834@slu.edu.ph>
2018-05-21 20:33:55 +0800

    updated

--- pages/owasp.php
+++ pages/owasp.php
@@ -43,17 +43,8 @@
 		<aside id="colorlib-hero">
 			<div class="flexslider">
 				<ul class="slides">
-			   	<li style="background-image: url(../images/cover_img_4.jpg);">
+			   	<li style="background-image: url(../images/owasp2.png);">
 			   		<div class="overlay"></div>
-			   		<div class="container">
-			   			<div class="row">
-				   			<div class="col-md-8 col-sm-12 col-xs-12 col-md-offset-2 text-center slider-text">
-				   				<div class="slider-text-inner">
-				   					<h1>OWASP Top 10 Web Application Security Risks</h1>
-				   				</div>
-				   			</div>
-				   		</div>
-			   		</div>
 			   	</li>
 			  	</ul>
 		  	</div>
