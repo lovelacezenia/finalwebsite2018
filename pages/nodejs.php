1d7b00d21906daf50efb291915dd3fba110fe0b1
GrahamFlora <grahamrossflora@gmail.com>
2018-05-21 23:32:30 +0800

    updated the notes in Node JS

--- pages/nodejs.php
+++ pages/nodejs.php
@@ -51,54 +51,56 @@
 		</aside>
 		<div id="colorlib-work">
 			<div class="container">
-				<button class="accordion">Injection</button>
+				<button class="accordion">Node JS</button>
 					<div class="panel">
-	  					<p>Injection flaws, such as SQL, NoSQL, OS, and LDAP injection, occur when untrusted data is sent to an interpreter as part of a command or query. The attacker's hostile data can trick the interpreter into executing unintended commands or accessing data without proper authorization.</p>
+	  					<p>Node.js is an open source server environment that is free and runs on various platforms (Windows, Linux, Unix, Mac OS X, etc.). It can open, read, write, delete files on the server. <p>
 					</div>
 
-				<button class="accordion">Broken Authentication</button>
+				<button class="accordion">Modules in Node JS</button>
 					<div class="panel">
-	  					<p>Application functions related to authentication and session management are often implemented incorrectly, allowing attackers to compromise passwords, keys, or session tokens, or to exploit other implementation flaws to assume other users' identities temporarily or permanently.</p>
+	  					<p>Node.js has a set of built-in modules which you can use without any further installation.
+						To include a module, use the require() function with the name of the module itself</p>
+						<p>Example: var http = require('http');</p>
 					</div>
 
-				<button class="accordion">Sensitive Data Exposure</button>
+				<button class="accordion"></button>
 					<div class="panel">
-	  					<p>Many web applications and APIs do not properly protect sensitive data, such as financial, healthcare, and PII. Attackers may steal or modify such weakly protected data to conduct credit card fraud, identity theft, or other crimes. Sensitive data may be compromised without extra protection, such as encryption at rest or in transit, and requires special precautions when exchanged with the browser.</p>
+	  					<p></p>
 					</div>					
 
-				<button class="accordion">XML External Entities (XXE)</button>
+				<button class="accordion"></button>
 					<div class="panel">
-	  					<p>Many older or poorly configured XML processors evaluate external entity references within XML documents. External entities can be used to disclose internal files using the file URI handler, internal file shares, internal port scanning, remote code execution, and denial of service attacks.</p>
+	  					<p></p>
 					</div>	
 
-				<button class="accordion">Broken Access Control</button>
+				<button class="accordion"></button>
 					<div class="panel">
 	  					<p>Restrictions on what authenticated users are allowed to do are often not properly enforced. Attackers can exploit these flaws to access unauthorized functionality and/or data, such as access other users' accounts, view sensitive files, modify other users' data, change access rights, etc.</p>
 					</div>	
 
-				<button class="accordion">Security Misconfiguration</button>
+				<button class="accordion"></button>
 					<div class="panel">
-	  					<p>Security misconfiguration is the most commonly seen issue. This is commonly a result of insecure default configurations, incomplete or ad hoc configurations, open cloud storage, misconfigured HTTP headers, and verbose error messages containing sensitive information. Not only must all operating systems, frameworks, libraries, and applications be securely configured, but they must be patched/upgraded in a timely fashion.</p>
+	  					<p></p>
 					</div>	
 
-				<button class="accordion">Cross-Site Scripting (XSS)</button>
+				<button class="accordion"></button>
 					<div class="panel">
-	  					<p>XSS flaws occur whenever an application includes untrusted data in a new web page without proper validation or escaping, or updates an existing web page with user-supplied data using a browser API that can create HTML or JavaScript. XSS allows attackers to execute scripts in the victim's browser which can hijack user sessions, deface web sites, or redirect the user to malicious sites.</p>
+	  					<p></p>
 					</div>	
 
-				<button class="accordion">Insecure Deserialization</button>
+				<button class="accordion"></button>
 					<div class="panel">
-	  					<p>Insecure deserialization often leads to remote code execution. Even if deserialization flaws do not result in remote code execution, they can be used to perform attacks, including replay attacks, injection attacks, and privilege escalation attacks.</p>
+	  					<p></p>
 					</div>
 
-				<button class="accordion">Using Components with Known Vulnerabilities</button>
+				<button class="accordion"></button>
 					<div class="panel">
-	  					<p>Components, such as libraries, frameworks, and other software modules, run with the same privileges as the application. If a vulnerable component is exploited, such an attack can facilitate serious data loss or server takeover. Applications and APIs using components with known vulnerabilities may undermine application defenses and enable various attacks and impacts.</p>
+	  					<p></p>
 					</div>
 
-				<button class="accordion">Insufficient Logging and Monitoring</button>
+				<button class="accordion"></button>
 					<div class="panel">
-	  					<p>Insufficient logging and monitoring, coupled with missing or ineffective integration with incident response, allows attackers to further attack systems, maintain persistence, pivot to more systems, and tamper, extract, or destroy data. Most breach studies show time to detect a breach is over 200 days, typically detected by external parties rather than internal processes or monitoring.</p>
+	  					<p></p>
 					</div>	
 			</div>
 		</div>
