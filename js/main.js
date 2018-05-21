4de25ec40bcaba8a693856ef6c891f7fcf172cf8
Lovelace Zennia Luisa Oliva <2144074@slu.edu.ph>
2018-05-21 16:17:24 +0800

    update developers page

--- js/main.js
+++ js/main.js
@@ -1,21 +1,30 @@
 ;(function () {
-	
+
 	'use strict';
 
 
 
-	// iPad and iPod detection	
+	// iPad and iPod detection
 	var isiPad = function(){
 		return (navigator.platform.indexOf("iPad") != -1);
 	};
 
 	var isiPhone = function(){
 	    return (
-			(navigator.platform.indexOf("iPhone") != -1) || 
+			(navigator.platform.indexOf("iPhone") != -1) ||
 			(navigator.platform.indexOf("iPod") != -1)
 	    );
 	};
-
+	// Document on load.
+	$(function(){
+		fullHeight();
+		burgerMenu();
+		counter();
+		sliderMain();
+		counterWayPoint();
+		contentWayPoint();
+		owlCrouselFeatureSlide();
+	});
 
 	var fullHeight = function() {
 
@@ -50,7 +59,7 @@
 		$('.animate-box').waypoint( function( direction ) {
 
 			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-				
+
 				i++;
 
 				$(this.element).addClass('item-animate');
@@ -69,16 +78,16 @@
 							el.removeClass('item-animate');
 						},  k * 200, 'easeInOutExpo' );
 					});
-					
+
 				}, 100);
-				
+
 			}
 
 		} , { offset: '85%' } );
 	};
 
 	var sliderMain = function() {
-		
+
 	  	$('#colorlib-hero .flexslider').flexslider({
 			animation: "fade",
 			slideshowSpeed: 5000,
@@ -111,9 +120,9 @@
 	var counterWayPoint = function() {
 		if ($('#colorlib-counter').length > 0 ) {
 			$('#colorlib-counter').waypoint( function( direction ) {
-										
+
 				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( counter , 400);					
+					setTimeout( counter , 400);
 					$(this.element).addClass('animated');
 				}
 			} , { offset: '90%' } );
@@ -139,22 +148,13 @@
 		      "<i class='icon-arrow-left3 owl-direction'></i>",
 		      "<i class='icon-arrow-right3 owl-direction'></i>"
 	     	]
-		});	
+		});
 	};
 
-	
 
 
-	// Document on load.
-	$(function(){
-		fullHeight();
-		burgerMenu();
-		counter();
-		sliderMain();
-		counterWayPoint();
-		contentWayPoint();
-		owlCrouselFeatureSlide();
-	});
+
+
 
 
 }());
