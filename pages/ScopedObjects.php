8f4316760bbc936d20083493861b2c3ad580c5ae
Sarah Mae Centino <2152834@slu.edu.ph>
2018-05-22 16:18:10 +0800

    update

--- pages/ScopedObjects.php
+++ pages/ScopedObjects.php
@@ -39,7 +39,12 @@
 <!--[if lt IE 9]>
 <script src="js/respond.min.js"></script>
 <![endif]-->
-
+<style>
+body{
+  background-image: url("../images/logo.png");
+  background-size: 50%;
+}
+</style>
 </head>
 <body>
 	<aside id="colorlib-hero">
